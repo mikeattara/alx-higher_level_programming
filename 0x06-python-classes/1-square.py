949b7c105408bc8593a88f289623fc4bb5c75b57
Mike Attara <mpyebattara@gmail.com>
2022-12-20 23:48:52 +0000

    model square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class"""
+
+
+class Square:
+    """ Model a Square
+        This class models a typical square
+    """
+
+    def __init__(self, size):
+        """Initialise a new square object with a size
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
